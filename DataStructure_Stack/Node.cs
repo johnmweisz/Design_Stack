b5275c9cd6d2e612885ca06d6f839e64bcd4a747
John Weisz <johnmweisz@gmail.com>
2019-12-20 14:38:13 -0800

    moved classes to seperate files

---
+++ DataStructure_Stack/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataStructure_Stack
+{
+    class Node
+    {
+        public object Val { get; }
+        public Node Next { get; set; }
+        public Node Previous { get; set; }
+
+        public Node(object val)
+        {
+            this.Val = val ?? throw new InvalidOperationException("Node value cannot be null");
+        }
+
+    }
+}
---
+++ DataStructure_Stack/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DataStructure_Stack
+{
+    class Stack
+    {
+        private Node _tail;
+
+        public Stack()
+        {
+            _tail = null;
+        }
+
+        public Stack(object val)
+        {
+            _tail = new Node(val);
+        }
+
+        public void Push(object val)
+        {
+            var newNode = new Node(val);
+            if (_tail == null) _tail = newNode;
+            else
+            {
+                newNode.Previous = _tail;
+                _tail.Next = newNode;
+                _tail = _tail.Next;
+            }
+        }
+
+        public object Pop()
+        {
+            if (_tail == null) throw new InvalidOperationException("Stack is empty");
+            var pop = _tail.Val;
+            _tail = _tail.Previous;
+            if (_tail != null) _tail.Next = null;
+            return pop;
+        }
+
+        public void Clear()
+        {
+            while (_tail != null)
+            {
+                _tail.Next = null;
+                _tail = _tail.Previous;
+            }
+        }
+    }
+}
